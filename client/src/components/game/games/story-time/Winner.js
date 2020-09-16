1f040fbd29d0113d8f87d02ee3fe665742ad8751
Jacob Garcia <jacobgarcia@aggienetwork.com>
2020-09-16 17:07:35 -0500

    styling

--- client/src/components/game/games/story-time/Winner.js
+++ client/src/components/game/games/story-time/Winner.js
@@ -3,6 +3,21 @@ import { connect } from 'react-redux';
 
 class Winner extends Component {
 
+  interval;
+  state = {
+    animateWinner: false
+  }
+
+  componentDidMount() {
+    this.interval = setTimeout(() => {
+      this.setState({animateWinner: true});
+    }, 300);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
     const { story, turn, winner } = this.props.gameState;
     return (
@@ -12,8 +27,10 @@ class Winner extends Component {
           <div className="speech-bubble speech-bubble-down slide-in-from-bottom">
             {story[turn + 1]}
           </div>
-          <div className="winner-name slide-in-from-top">
-            {winner}
+          <div className="winner-name-container">
+            {this.state.animateWinner ? <div className="winner-name slide-in-from-top">
+              {winner}
+            </div> : null}
           </div>
         </div>
       </div>
